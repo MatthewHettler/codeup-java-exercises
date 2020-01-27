73a172f0e8df631229c54e48cdab2bebec56e8f0
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-27 16:58:36 -0600

    Finished the Console Exercises.

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+//        double pi = 3.14159;
+////      The value of pi is approximately 3.14.
+//        System.out.format("The value of pi is approximately %.2f.", pi);
+
+//        SCANNER CLASS EXAMPLE
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter an integer: ");
+//        Integer userInput = scanner.nextInt();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+//        SCANNER CLASS EXAMPLE 2
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter 3 words: ");
+//        String userInput = scanner.next();
+//        String userInput2 = scanner.next();
+//        String userInput3 = scanner.next();
+//        System.out.println("You entered: --> \"" + userInput + ", " + userInput2 + ", " + userInput3 + "\" <--");
+
+//        SCANNER CLASS EXAMPLE 3
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a sentence: ");
+//        String userInput = scanner.nextLine();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+//        SCANNER CLASS EXAMPLE 4
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter values of length and width of a classroom at Codeup: ");
+//        Integer length = scanner.nextInt();
+//        Integer width = scanner.nextInt();
+//        System.out.println("The area of the Codeup classroom is: --> \"" + length*width + "\" <--");
+//        System.out.println("The perimeter of the Codeup classroom is: --> \"" + ((2*length)+(2*width)) + "\" <--");
+
+
+
+
+
+
+
+    }
+}
