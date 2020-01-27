797f9d2deaad279437584907c36f9c4bb4473faa
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-27 14:00:13 -0600

    Added syntax,types, and variables exercises.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,64 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+//        System.out.println("Hello World!");
+//
+//        int myFavoriteNumber = 10;
+//        System.out.println((myFavoriteNumber));
+//
+//        String myString = "Matthew";
+//        System.out.println((myString));
+//
+//        float myNumber = 123;
+//        System.out.println((myNumber));
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        // Produces an error
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+        // Produces an error
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+
+              // Using the shorthand arithmetic assignment operators
+//            int x = 4;
+//            x+=5;
+//            System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(y);
+
+              // Using the shorthand arithmetic assignment operators
+//            int x = 3;
+//            int y = 4;
+//            y*=x;
+//            System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        System.out.println(y);
+
+              // Using the shorthand arithmetic assignment operators
+//            int x = 10;
+//            int y = 2;
+//            x /= y;
+//            y -= x;
+//            System.out.println(y);
+
     }
 }
