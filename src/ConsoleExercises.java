ac5dde557151aa29fd5901ce4dd676b1deb979ae
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-28 09:54:03 -0600

    Finished walkthrough.

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -13,6 +13,15 @@ public class ConsoleExercises {
 //        Integer userInput = scanner.nextInt();
 //        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
+//        Walk-through version.
+//        Scanner scanner = new Scanner(System.in);
+//        int i = 0;
+//        while (i != -1) {
+//            System.out.print("Enter an integer: ");
+//            i = scanner.nextInt();
+//            System.out.println("\nThe number is " + i + ".\n");
+//        }
+
 //        SCANNER CLASS EXAMPLE 2
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Enter 3 words: ");
@@ -27,6 +36,13 @@ public class ConsoleExercises {
 //        String userInput = scanner.nextLine();
 //        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
+//        Walk-through version.
+//        System.out.print("\nEnter a sentence: ");
+//        scanner.useDelimiter("\n");
+//        String sentence = scanner.next();
+//        scanner.nextLine();
+//        System.out.printf("\nThe sentence is \"%s\"\n", sentence);
+
 //        SCANNER CLASS EXAMPLE 4
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Enter values of length and width of a classroom at Codeup: ");
@@ -34,6 +50,24 @@ public class ConsoleExercises {
 //        Integer width = scanner.nextInt();
 //        System.out.println("The area of the Codeup classroom is: --> \"" + length*width + "\" <--");
 //        System.out.println("The perimeter of the Codeup classroom is: --> \"" + ((2*length)+(2*width)) + "\" <--");
+
+//        Both the last two chunks of code are Walk-through versions.
+//        Scanner scan = new Scanner(System.in);
+////        scanner.useDelimiter("\n");
+//        System.out.print("\nEnter the width: ");
+//        String sWidth = scan.nextLine();
+//        double width = Double.parseDouble(sWidth);
+//        System.out.print("Enter the length: ");
+//        String sLength = scan.nextLine();
+//        double length = Double.parseDouble(sLength);
+//        System.out.printf("The dimensions are %f by %f. The perimeter is %f and the area is %f. \n", length, width, 2*(length+width), length*width);
+
+//        scanner.useDelimiter("\n");
+//        System.out.print("\nHey! Yo, enter the width: ");
+//        double width = scanner.nextFloat();
+//        System.out.print("Enter the length: ");
+//        double length = scanner.nextFloat();
+//        System.out.printf("The dimensions are %f by %f. The perimeter is %f and the area is %f.\n", length, width, 2*(length+width), length*width);
 
     }
 }
