91a2e245add08f37cc35e6f28383d27c3e31b170
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-29 11:32:01 -0600

    Added stringexercise and bob java classes.

---
+++ src/StringExercise.java
@@ -0,0 +1,20 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+//      ***** Exercise 1: String Basics *****
+//        String message = "We don't need no";
+//        System.out.println(message + " education"); // prints "We don't need no education"
+//        message = message + " thought control";
+//        System.out.println(message); // prints "We don't need no thought control"
+
+//        String message = "Check \"this\" out!, \"s inside of \"s!\n";
+//        System.out.println(message);
+
+//        String message = "In windows, the main drive is usually C:\\";
+//        System.out.println(message);
+
+//        String message = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\!";
+//        System.out.println(message);
+
+    }
+}
