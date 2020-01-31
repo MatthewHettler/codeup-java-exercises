d595705240ba1c8461339fbe5251599a8f9608d0
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-31 10:06:13 -0600

    Added student.java class.

---
+++ src/Student.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+
+public class Student {
+    String name;
+    String cohort;
+    Date startDate;
+    String program;
+    String location;
+    boolean drinksCoffee;
+
+    public static void main(String[] args) {
+        Student matthew = new Student();
+        matthew.name = "Matthew";
+        matthew.drinksCoffee = true;
+        matthew.cohort = "Europa";
+        matthew.startDate = new Date("11/4/2019");
+        matthew.program = "Web Development";
+        matthew.location = "San Antonio";
+
+        Student miguel = new Student();
+        miguel.name = "Miguel";
+        miguel.cohort = "Europa";
+        miguel.startDate = new Date("November 4, 2019");
+        miguel.program = "Web Development";
+        miguel.location = "San Antonio";
+        miguel.drinksCoffee = false;
+
+        System.out.println(matthew.report());
+        System.out.println(miguel.report());
+        if (matthew.drinksCoffee)
+            System.out.println(matthew.name + " drinks coffee.");
+    }
+
+    public String report() {
+        String output = "";
+        output += "My name is " + this.name +". ";
+        output += "I started learning " + this.program + " with " + this.cohort + " on " + this.startDate + ". ";
+        if (this.drinksCoffee)
+            output += "I drink coffee in " + this.location + ".";
+            else
+                output += "I don't drink coffee.";
+            return output;
+    }
+}
