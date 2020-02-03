fb7d3f39a46c9582f17fb85c642068301691b771
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-02-03 09:28:45 -0600

    Added sumOfBinaryNumbers method.

--- src/ProgrammingPushups.java
+++ src/ProgrammingPushups.java
@@ -1,7 +1,9 @@
 public class ProgrammingPushups {
     public static void main(String[] args){
-        System.out.println(multiply(5, 5));
-        System.out.println(toThePower(5, 5));
+//        System.out.println(multiply(5, 5));
+//        System.out.println(toThePower(5, 5));
+        System.out.println(sumOfBinaryNumbers("10", "11"));
+        System.out.println(sumOfBinaryNumbers("1010", "1100"));
     }
 
     public static int multiply (int a, int b){
@@ -18,6 +20,13 @@ public class ProgrammingPushups {
             product *= a;
         }
         return product;
+    }
+
+    public static String sumOfBinaryNumbers (String first, String second) {
+        int b1 = Integer.parseInt(first, 2);
+        int b2 = Integer.parseInt(second, 2);
+        int sum = b1 + b2;
+        return Integer.toBinaryString(sum);
     }
 
 }
