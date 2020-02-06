f4dab7b7cbf1b3bd34f55b6602bcc1227c484dd0
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-02-06 11:56:33 -0600

    Added some changes to the println text.

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -16,14 +16,13 @@ public class GradesApplication {
         Students.put("FernandoGarza", new Student("Fernando", 70));
         Students.put("JustinWorthy", new Student("Justin", 80));
 
-        System.out.println("Welcome!\n");
-        System.out.println("Here are the GitHub usernames of our students: \n");
-
         doStuff();
     }
 
     public static void doStuff() {
 
+        System.out.println("Welcome!\n");
+        System.out.println("Here are the GitHub usernames of our students: \n");
         System.out.println(Students.keySet() + "\n");
         System.out.println("What student would you like to see more information on?\n");
         Scanner scanner = new Scanner(System.in);
@@ -31,7 +30,7 @@ public class GradesApplication {
 
         if (!Students.containsKey(option)) {
             System.out.println("Sorry, no student found with the GitHub username of " + "\"" + option + "\"." + "\n");
-            System.out.println("\nWould you like to see another student? y/n ");
+            System.out.println("\nWould you like to see another student?\n");
             boolean confirm = userInput.yesNo();
             if (confirm) {
                 doStuff();
@@ -39,8 +38,8 @@ public class GradesApplication {
                 System.out.println("Goodbye, and have a wonderful day!");
             }
         } else if (Students.containsKey(option)) {
-            System.out.printf("\nName: %S - GitHub Username: %S - Current Average: %f", Students.get(option).getName(), option, Students.get(option).getGradeAverage());
-            System.out.println("\nWould you like to see another student? y/n ");
+            System.out.printf("\nName: %S - GitHub Username: %S - Current Average: %f\n", Students.get(option).getName(), option, Students.get(option).getGradeAverage());
+            System.out.println("\nWould you like to see another student?\n");
             boolean confirm = userInput.yesNo();
             if (confirm) {
                 doStuff();
